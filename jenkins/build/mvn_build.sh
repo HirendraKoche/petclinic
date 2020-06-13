4f51cc545cdb002d53bc249d36799fb7afc18fa8
hirendra <hirendrakoche1@outlook.com>
2020-06-13 18:41:07 +0000

    Test1

--- jenkins/build/mvn_build.sh
+++ jenkins/build/mvn_build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo $PWD
-docker container run --rm -v $PWD:/app -v $JENKINS_HOME/.m2:/root/.m2 -w /app maven:3-alpine $@
+
+
+
+docker container run --rm -v $JENKINS_HOME/workspace/Pipeline_Petclinic:/app -v $JENKINS_HOME/.m2:/root/.m2 -w /app maven:3-alpine $@
