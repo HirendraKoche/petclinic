1598a2fcb2c2ed831396cc8d2cc80f0b314129e4
hirendra <hirendrakoche1@outlook.com>
2020-06-13 19:34:36 +0000

    Test

--- jenkins/build/mvn_build.sh
+++ jenkins/build/mvn_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-WORKSPACE=/var/lib/docker/volumes/jenkins_jenkins_home/_data/
+JENKINS_CONTAINER=$(docker container ls -q --filter name=_jenkins)
 
-docker container run --rm -v $WORKSPACE/workspace/Pipeline_Petclinic:/app -v $WORKSPACE/.m2:/root/.m2 -w /app maven:3-alpine $@
+docker container run --rm --volume-from=$JENKINS_CONTAINER -w $JENKINS_HOME/workspace/Pipeline_Petclinic $HOME/.m2:/root/.m2 -w /app maven:3-alpine $@
